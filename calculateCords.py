1f1ee074909ffcb7f9cd2d793c0bcb7077afeeeb
es <zukulisemils@gmail.com>
2020-06-29 17:42:40 +0300

    initial commit

---
+++ calculateCords.py
@@ -0,0 +1,21 @@
+from koordinates import LKSToLatLon
+import json
+
+file = 'LaukiGeojson/Dizzoni_3.geojson'
+
+def cords(file):
+    cords = []
+    with open(file) as f:
+        data = json.load(f)
+    for feature in data['features']:
+        cords =  feature['geometry']['coordinates'][0]
+        i = 0
+        while len(cords) > i:
+            x = []
+            x += LKSToLatLon(cords[i][0], cords[i][1])
+            i += 1
+            print(x,', ')
+        print('\n')
+    return cords
+
+cords(file)
