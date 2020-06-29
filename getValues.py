405007e6358880e3b3bb41ec78da9aa3722d7200
es <zukulisemils@gmail.com>
2020-06-29 14:58:03 +0300

    fix

--- getValues.py
+++ getValues.py
@@ -13,11 +13,27 @@ with open(CSV_FILE, encoding='utf8', newline='') as f:
     lines = list(reader)
 
 for i, feature in enumerate(geo_data['features']):
+    id = feature['properties']['ID']
+    print(id)
     pH = feature['properties']['pH']
-    if i < len(lines):
-        lines[i][1] = pH
+    p = feature['properties']['P']
+    k = feature['properties']['K']
+    mg = feature['properties']['Mg']
+    if i+1 < len(lines):
+
+        lines[0][0] = 'ID'
+        lines[0][1] = 'pH'
+        lines[0][2] = 'P'
+        lines[0][3] = 'K'
+        lines[0][4] = 'Mg'
+
+        lines[i+1][0] = id
+        lines[i+1][1] = pH
+        lines[i+1][2] = p
+        lines[i+1][3] = k
+        lines[i+1][4] = mg
     else:
-        lines.append([1, pH, 1, 1, 1])
+        lines.append([id, pH, p, k, mg])
 
 with open(CSV_FILE, 'w', encoding='utf8', newline='') as f:
     writer = csv.writer(f, delimiter=',')
